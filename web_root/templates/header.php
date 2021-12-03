11f485878c16d9c42f084e4d3eb6b149c94fdfec
Redjinator <71242881+Redjinator@users.noreply.github.com>
2021-12-02 18:06:36 -0800

    Add: header and footer empty templates in templates folder

---
+++ web_root/templates/header.php
@@ -0,0 +1,8 @@
+<!--
+    Student:        Reginald McPherson
+    Course:         DGL-123 PHP Fundamentals
+    Last Modified:  Dec 02, 2021
+    Filename:       header.php
+    Purpose:        Part of the course final project.
+-->
+
