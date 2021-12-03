9a0684b3c5296e8d12c0a3f87b29bdcace5c5529
Redjinator <71242881+Redjinator@users.noreply.github.com>
2021-12-03 04:19:20 -0800

    Fix: incorrect values that were entered in error while referencing course textbook

--- web_root/templates/footer.php
+++ web_root/templates/footer.php
@@ -6,8 +6,8 @@
 if((is_admin() && (basename($_SERVER['PHP_SELF']) != 'logout.php')) OR (isset($loggedin) && $loggedin)) {
     
     // Create links
-    print '<hr><h3>Site Admin</h3><p><a href="add_quote.php">Add Quote</a> <->
-    <a href="view_quotes.php">View All Quotes</a> <->
+    print '<hr><h3>Admin commands</h3><p><a href="add_item.php">Add Food</a> <->
+    <a href="view_items.php">View Items</a> <->
     <a href="logout.php">Logout</a></p>';
 }
 
