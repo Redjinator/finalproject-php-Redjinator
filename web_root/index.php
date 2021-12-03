352426c43e264ac93d73625cba351240eb42aba9
Redjinator <71242881+Redjinator@users.noreply.github.com>
2021-12-03 06:11:04 -0800

    Update: to-do list

--- web_root/index.php
+++ web_root/index.php
@@ -6,7 +6,10 @@
 <ul style="margin-bottom: 1rem;">
     <li>Make a mock up front page</li>
     <li>Create a meal plane page</li>
-    <li>My db entries don't check for duplicates yet</li>
+    <li class="error">My db entries don't check for duplicates yet</li>
+    <li class="bg-success">public facing meal plan, 5 days, 20 food slots (4 food 1 drink per meal/snack)</li>
+    <li>Add to schedule page, uses drop downs based on user selection to enter items onto the meal plan slots</li>
+    <li>Use combo of bootstrap and sass for document style</li>
 </ul>
 
 <h2>File paths test</h2>
