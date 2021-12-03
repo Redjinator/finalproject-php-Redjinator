acc06e26039a90e1fb5dbbd7bd9a2599210693e1
Redjinator <71242881+Redjinator@users.noreply.github.com>
2021-12-02 18:09:18 -0800

    Add: file initialize.php, file path constants

---
+++ web_root/includes/initialize.php
@@ -0,0 +1,15 @@
+<?php
+
+    // Constants for file paths, hopefully to avoid errors
+    define("PRIVATE_PATH", dirname(__FILE__));
+    define("PROJECT_PATH", dirname(PRIVATE_PATH));
+    define("PUBLIC_PATH", PROJECT_PATH . '\public');
+    define("TEMPLATE_PATH", PRIVATE_PATH . '\templates');
+
+    // root url to constant, checks that public is in the path
+    $public_end = strpos($_SERVER['SCRIPT_NAME'], '\public') + 7;
+    $doc_root = substr($_SERVER['SCRIPT_NAME'], 0, $public_end);
+    define("WWW_ROOT", $doc_root);
+
+
+?>
