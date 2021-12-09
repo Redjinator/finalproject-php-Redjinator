c61e9faad91e4b2db24fd1dc19a3a2f5ee8566a4
Redjinator <71242881+Redjinator@users.noreply.github.com>
2021-12-08 16:48:59 -0800

    Add: custom nav menu

--- web_root/templates/footer.php
+++ web_root/templates/footer.php
@@ -1,26 +1,35 @@
 <?php
-
-// Display admin links if the user is admin & it's not logout.php page
-// or if the $loggedin variable is true (i.e., the user just logged in)
-
+/* ADMIN ONLY LINKS */
 if((is_admin() && (basename($_SERVER['PHP_SELF']) != 'logout.php')) OR (isset($loggedin) && $loggedin)) {
-    
+
     // Create links
-    print '<hr><h3>Admin commands</h3><p><a href="add_item.php">Add Food</a> <->
-    <a href="view_items.php">View Items</a> <->
-    <a href="meal_plan.php">Meal Plan</a> <->
+    print '<hr><h3>Admin commands</h3><p><a href="add_item.php">Add Food</a> |
+    <a href="view_items.php">View Items</a> |
+    <a href="meal_plan.php">Meal Plan</a> |
     <a href="logout.php">Logout</a></p>';
 }
-
 ?>
 
+<!--FOOTER
+=======================================================-->
+<div id="footer">
+    <p class="">Content &copy; 2021</p>
+</div> <!-- end of footer -->
 
-    <div id="footer">
-
-        <p class="footer__paragraph">Content &copy; 2021</p>
-
-    </div> <!-- end of footer -->
 </div><!-- main -->
+
+
+<!-- Bootstrap related scripts -->
+<script src="https://code.jquery.com/jquery-3.0.0.min.js"></script>
+<script src="../js/popper.min.js"></script>
+<script src="../js/bootstrap.min.js"></script>
+
+<script>
+	$(function () {
+		$('[data-toggle="popover"]').popover();
+	})
+</script>
+
 
 </body>
 </html>
--- web_root/templates/header.php
+++ web_root/templates/header.php
@@ -14,37 +14,67 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>LH&H - <?php echo $page_title ?></title>
-    <!-- <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous"> -->
+
+
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
+
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@5.15.4/css/fontawesome.min.css" integrity="sha384-jLKHWM3JRmfMU0A5x5AkjWkw/EYfGUAGagvnfryNV3F9VqM98XiIH7VBGVoxVSc7" crossorigin="anonymous">
-    <link rel="stylesheet" href="main.css">
-
+    <link rel="stylesheet" href="../main.css">
+    <link rel="stylesheet" href="../menu.css">
 </head>
+
+
 <body>
-<header>
 
-    <!-- Nav menu -->
-    <nav class="nav">
-        <ul class="menu-nav">
+<nav class="navbar">
+  <ul class="navbar-nav">
 
-            <li class="menu-nav__item">
-                <a href="index.php"     class="menu-nav__link">Home</a>
-            </li>
+    <li class="logo nav-item">
+      <a href="/index.php" class="nav-link">
+        <span class="logo-text">Home</span>
+        <img src="./img/arrow-right-solid.svg" alt="home">
+      </a>
+    </li>
 
-            <li class="menu-nav__item">
-                <a href="add_item.php"  class="menu-nav__link">Add Item</a>
-            </li>
+    <li class="nav-item">
+      <a href="/login.php" class="nav-link">
+        <img src="./img/house-user-solid.svg" alt="About me">
+        <span class="link-text">Sign In</span>
+      </a>
+    </li>
 
-            <li class="menu-nav__item">
-                <a href="meal_plan.php" class="menu-nav__link">Meal Plan</a>
-            </li>
+    <li class="nav-item">
+      <a href="/add_item.php" class="nav-link">
+        <img src="/img/plus.svg" alt="View items">
+        <span class="link-text">Add Food</span>
+      </a>
+    </li>
 
-            <li class="menu-nav__item">
-                <a href="logout.php"    class="menu-nav__link">Logout</a>
-            </li>
+    <li class="nav-item ">
+      <a href="/view_items.php" class="nav-link">
+        <img src="./img/tasks-solid.svg" alt="View items">
+        <span class="link-text">View All Items</span>
+      </a>
+    </li>
 
-        </ul>
-    </nav> <!-- End nav -->
-</header> <!-- End header -->
+    <li class="nav-item ">
+      <a href="/meal_plan.php" class="nav-link">
+        <img src="./img/tasks-solid.svg" alt="View items">
+        <span class="link-text">Weekly Meal Plan</span>
+      </a>
+    </li>
+
+    <li class="nav-item ">
+      <a href="/logout.php" class="nav-link">
+        <img src="../img/sign-out-alt-solid.svg" alt="log out">
+        <span class="link-text">Sign Out</span>
+      </a>
+    </li>
+
+  </ul>
+</nav> <!-- #endregion -->
+
+<div class="main">
 <!-- BEGIN CHANGEABLE CONTENT -->
 
 
